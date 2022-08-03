269f2efc4acb07727ab6224899068093ebafe229
Bryan Matias <matiasbryan@gmail.com>
2022-08-02 17:46:34 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const request = require('request')
+const path = require('path')
+
+const baseUrl = 'http://bryansandbox.imgix.net/'
+// const baseUrl = 'https://your_base_url_here'
+
+app.get('/', (req, res) => {
+    res.status(200).send('Homepage')
+})
+
+app.get('/*', (req, res) => {
+
+    let url = baseUrl + req.originalUrl;
+
+    // proxy the request and send it
+    request({
+        url: url,
+        encoding: null
+    },
+        (err, resp, buffer) => {
+            if (!err && resp.statusCode === 200) {
+                // if we get a 200 response
+                res.set(resp.headers)
+                // sets the haeders;
+                res.send(resp.body);
+            } else {
+                // if error. You can customise this error here.
+                res.status(resp?.statusCode).send(resp?.statusMessage)
+            }
+        });
+
+})
+
+app.listen(3000, () => console.log(`Example app listening on port ${3000} - Dir name is ${__dirname}`))
