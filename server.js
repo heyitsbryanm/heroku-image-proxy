89d8193e3fa754eea4280193f47f45b20ff2e139
Bryan Matias <matiasbryan@gmail.com>
2022-08-02 18:02:46 -0700

    Adds dotenv

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const request = require('request')
 const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config();
 
 // define this in your .env or your environment variables
-const baseUrl = process.env.baseUrl
+const baseUrl = process.env.BASEURL
 
 app.get('/', (req, res) => {
     res.status(200).send('Homepage')
@@ -13,6 +15,7 @@ app.get('/', (req, res) => {
 app.get('/*', (req, res) => {
 
     let url = baseUrl + req.originalUrl;
+    console.log(url)
 
     // proxy the request and send it
     request({
